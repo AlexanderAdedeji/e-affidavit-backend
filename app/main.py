57dd606c43672d8fbeb0f022cd77b0bfc271419c
AlexAdedeji <alex.adedeji@identikosolutions.com>
2024-03-08 11:02:48 +0000

    main update

--- app/main.py
+++ app/main.py
@@ -93,4 +93,4 @@ async def shutdown_db_client():
 
 
 if __name__=="__main__":
-    uvicorn.run("main:app",host="0.0.0.0",port=4100,reload=True)
+    uvicorn.run("app.main:app",host="0.0.0.0",port=4100,reload=True)
