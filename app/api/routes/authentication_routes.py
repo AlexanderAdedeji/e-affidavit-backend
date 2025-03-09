7fd1d3a23740abd2f19addc9205ec48edbeff0ad
AlexAdedeji <alexadedeji15@gmail.com>
2025-03-08 20:20:57 -0800

    commissioner security

--- app/api/routes/authentication_routes.py
+++ app/api/routes/authentication_routes.py
@@ -133,7 +133,7 @@ def login(
             detail="Your account is not verified. Check your mail for a new verification email."
         )
 
-        if user.user_type.name == settings.COMMISSIONER_USER_TYPE:
+    if user.user_type.name == settings.COMMISSIONER_USER_TYPE:
             validate_commissioner_device(user, user_login, db, background_task)
     try:
         token = user.generate_jwt()
